66832b2e22476f5e52414556ff842bed1e2ac621
3liyounis <aliyounis642@gmail.com>
2025-10-27 13:11:05 +0200

    Integrate Server With Firebase Realtime Database & Firestore

--- Server/src/config/firebase.ts
+++ Server/src/config/firebase.ts
@@ -1,9 +1,10 @@
-import admin from 'firebase-admin';
-var serviceAccount = require("../../serviceAccount.json");
+import { firebaseConfig } from './firebaseConfig';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database'
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+const app = initializeApp(firebaseConfig);
+const firestoreDB = getFirestore(app)
+const realtimeDB = getDatabase(app)
 
-export const db = admin.firestore();
-export default admin;
+export default {firestoreDB, realtimeDB};
